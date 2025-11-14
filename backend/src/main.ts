6e82cc44af3926fd81431d96ee97e16325900bc1
brunope233 <paraisoreggae@gmail.com>
2025-11-14 10:52:08 -0300

    fix: Atualiza política de CORS para produção

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Lista de origens permitidas
+  const whitelist = [
+    'http://localhost:3000', // Para desenvolvimento local
+    'https://evolve-platform-478121.web.app', // A URL do seu frontend em produção
+  ];
+
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: function (origin, callback) {
+      // Permite requisições sem 'origin' (como de apps mobile ou Postman)
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Não permitido pela política de CORS'));
+      }
+    },
   });
 
   app.setGlobalPrefix('api/v1');
