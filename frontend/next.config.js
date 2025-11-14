9638906602db05ce51b189f9feeb0f77eea6bf86
brunope233 <paraisoreggae@gmail.com>
2025-11-14 14:31:45 -0300

    fix: Finaliza configuração de URLs do GCS para produção

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Define as variáveis de ambiente que estarão disponíveis tanto no servidor quanto no cliente
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_GCS_URL: process.env.NEXT_PUBLIC_GCS_URL,
+  },
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
-        // O pathname precisa ser mais genérico para aceitar qualquer bucket
-        pathname: `/**`, 
+        pathname: `/${process.env.NEXT_PUBLIC_GCS_BUCKET_NAME}/**`,
       },
     ],
   },
